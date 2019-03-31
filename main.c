1900882ee3e2438ea3f3073b65ee79aa0e593032
Junhao Liao <junhao@junhao.ca>
2019-03-31 16:01:10 -0400

    Initial commit

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int count =100;
+    int ages[count];
+    for(int i=0;i<count;i++){
+        ages[i]=0;
+    }
+    ages[3]=6;
+    ages[6]=101;
+    ages[50]=100;
+    ages[100]=99;
+    int oldest=0;
+    int secondOldest=0;
+
+    for(int i=0; i<count;i++){
+        if(ages[i]>oldest){
+            secondOldest=oldest;
+            oldest=ages[i];
+        } else if(ages[i]!=oldest&&ages[i]>secondOldest){
+            secondOldest=ages[i];
+        }
+    }
+
+    printf("%d",secondOldest);
+
+    return 0;
+}
